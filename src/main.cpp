7b5ded324dd0252786730abece3c089a0b2ddb94
Luiz Henrique Silva Sampaio <luiz@lsamp.dev>
2026-09-11 10:46:16 -0300

    feat: add verbose mode cli argument checker

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <string>
 
-int main(int argc, char* /*argv*/[]) {
+int main(int argc, char* argv[]) {
     constexpr int requiredArgC = 5;
     if (argc < requiredArgC) {
         std::cout
             << "Usage: pesquisa <method> <quantity> <situation> <key> [-P]";
+    }
+
+    constexpr int optionalLastArgPos = 5;
+    if (argc == (optionalLastArgPos + 1) &&
+        std::string(argv[optionalLastArgPos]) == std::string("-P")) {
+        // TODO: Set verbose mode
     }
 
     return 0;
